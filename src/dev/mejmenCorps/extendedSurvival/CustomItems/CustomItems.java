8fb0f553aa3a9fd5c823834909eb0a91307f9dde
krzyb5081 <>
2018-03-29 17:16:48 +0200

    To make custom items

--- src/dev/mejmenCorps/extendedSurvival/CustomItems/CustomItems.java
+++ src/dev/mejmenCorps/extendedSurvival/CustomItems/CustomItems.java
@@ -1,1 +1,6 @@
-To write
+package dev.mejmenCorps.extendedSurvival.CustomItems;
+
+public class CustomItems {//here is place where we add custom items needed to make something in plugin like backpack
+    
+    
+}
--- src/dev/mejmenCorps/extendedSurvival/Events/Backpack/BackpackOpen.java
+++ src/dev/mejmenCorps/extendedSurvival/Events/Backpack/BackpackOpen.java
@@ -4,7 +4,9 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
 
 public class BackpackOpen implements Listener {
-    public void openBackpackByRightClick(){
-        
+    public void openBackpackByRightClick(PlayerInteractEvent event){
+        if(event.getItem().equals(/*custom backpack item here*/){
+            
+        }
     }
 }
