7dfba091b2de9bd5b2472a3a10d81200043ac58f
krzyb5081 <EraDinozaurow@EraDinozaurow13>
2018-03-29 02:10:29 +0200

    To make custom items

--- src/dev/mejmenCorps/extendedSurvival/CustomItems/CustomItems.java
+++ src/dev/mejmenCorps/extendedSurvival/CustomItems/CustomItems.java
@@ -1,13 +1,1 @@
-/*
- * Here comes the text of your license
- * Each line should be prefixed with  * 
- */
-package dev.mejmenCorps.extendedSurvival.CustomItems;
-
-/**
- *
- * @author EraDinozaurow
- */
-public class CustomItems {
-    
-}
+To write
