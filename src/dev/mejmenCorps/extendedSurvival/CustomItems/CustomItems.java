0ff62dfb71b9776df3d3e2a681f67cf7b8c36e52
krzyb5081 <37555471+krzyb5081@users.noreply.github.com>
2018-04-01 17:34:11 +0000

    Delete CustomItems.java

--- src/dev/mejmenCorps/extendedSurvival/CustomItems/CustomItems.java
+++
@@ -1,6 +0,0 @@
-package dev.mejmenCorps.extendedSurvival.CustomItems;
-
-public class CustomItems {//here is place where we add custom items needed to make something in plugin like backpack
-    
-    
-}
