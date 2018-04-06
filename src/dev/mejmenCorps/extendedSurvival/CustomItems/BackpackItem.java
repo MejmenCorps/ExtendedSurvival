20727001664f87a7a369d9e5e410962e9dfc6028
krzyb5081 <EraDinozaurow@EraDinozaurow13>
2018-04-06 19:30:49 +0200

    Just commit to make my job on other computer :o

---
+++ src/dev/mejmenCorps/extendedSurvival/CustomItems/BackpackItem.java
@@ -0,0 +1,15 @@
+package dev.mejmenCorps.extendedSurvival.CustomItems;
+
+//import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemStack;
+
+// u pick the wrong house fool
+public class BackpackItem {
+    public void backpack(){
+        ItemStack backpackItem = new ItemStack(Material.CHEST);
+        backpackItem.addUnsafeEnchantment(Enchantment.LUCK, 69);
+        backpackItem.set
+    }
+}
